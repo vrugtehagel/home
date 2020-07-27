d21c156ce0a16b39843d9f3404a82f53b74c08d6
vrugtehagel <vrugtehagel@gmail.com>
2020-07-27 08:42:35 +0200

    got to wall of fame

--- general.js
+++ general.js
@@ -7,10 +7,28 @@ document.addEventListener('DOMContentLoaded', function(){
 			const minutes = date.getMinutes();
 			const t = (hours * 60 + minutes) / 720;
 			const time = t < 1 ? 1 - t : t - 1;
-			const svg = document.querySelector('#intro svg');
-			svg.style.setProperty('--time', time);
+			document.body.style.setProperty('--time', time);
 		};
 		setTime();
 		setInterval(setTime, 60000);
+	})();
+
+	// set cave entering
+	(() => {
+		let entered = false;
+		const caveEntrance = document.getElementById('cave-entrance');
+		const getThreshold = () => caveEntrance.offsetTop + caveEntrance.offsetHeight - window.innerHeight;
+		window.addEventListener('scroll', event => {
+			const threshold = getThreshold();
+			console.log(window.scrollY, threshold, entered);
+			if(window.scrollY < threshold && entered){
+				entered = false;
+				caveEntrance.classList.remove('entered');
+			}
+			else if(window.scrollY > threshold && !entered){
+				entered = true;
+				caveEntrance.classList.add('entered');
+			}
+		});
 	})();
 });
